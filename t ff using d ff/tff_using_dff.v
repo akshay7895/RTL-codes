f3e06de2f0bde11268c0a479892cc4a6f7f5bbf1
akshay purohit <45726518+akshay7895@users.noreply.github.com>
2023-05-14 15:45:06 +0530

    Update tff_using_dff.v
    finally able to write bug-free code.

--- t ff using d ff/tff_using_dff.v
+++ t ff using d ff/tff_using_dff.v
@@ -1,23 +1,26 @@
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 
-module tff_using_dff( input t, reset, clk, output  q, output qbar);
+module tff(input t, clk, reset, output q, qbar);
+
     wire tmp;
-    assign tmp = t ^ q;
-
-    dff dut (.data(tmp), .reset(reset), .clk(clk), .q(q), .qbar(qbar));
-
-
+    wire op,op1;
+    assign tmp = t^q;
+    dff dut (.d(tmp), .clk(clk), .reset(reset), .q(op), .qbar(op1));
+    assign q = op;
+    assign qbar = op1;
 endmodule
 
-module dff (input data, reset, clk, output reg q, output qbar);
-
+module dff(input d, clk, reset, output reg q, qbar);
     always@(posedge clk)
     begin
-        if(reset)
+        if(reset) begin
             q <= 0;
+            qbar <= 1;
+        end
         else
-            q <= data;
+            begin
+                q <= d;
+                qbar <= ~d;
+            end
     end
-
-    assign qbar = ~q;
 endmodule
