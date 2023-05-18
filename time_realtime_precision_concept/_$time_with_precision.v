04769f84d8691a4bbd1fec8ee0adc090384c5763
akshay purohit <45726518+akshay7895@users.noreply.github.com>
2023-05-18 21:51:42 +0530

    Create _$time_with_precision.v
    NOTE: 
    1) first the time delay value converted to nearest decimal value.
    2) then the precision value in added with them.(here precision is 10ps, so 00 added.
    
    output-->
    
    time=                   0, clk=0
    time=                1600, clk=1
    time=                3200, clk=0
    time=                4700, clk=1
    time=                6300, clk=0
    time=                7900, clk=1

---
+++ time_realtime_precision_concept/_$time_with_precision.v
@@ -0,0 +1,12 @@
+// Code your design here
+`timescale 1ns/10ps
+
+module new ;
+reg clk=0;
+  always #15.757 clk = ~clk;
+  initial
+    begin
+      $monitor("time=%t, clk=%b",$time,clk);
+      #200 $finish;
+    end
+endmodule
