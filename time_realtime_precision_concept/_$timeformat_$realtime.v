0a4b559c513c23a2f907580b2adee4ce474bd90f
akshay purohit <45726518+akshay7895@users.noreply.github.com>
2023-05-18 22:07:09 +0530

    Create _$timeformat_$realtime.v
    NOTE: $timeformat used.
    and because of $realtime, it is giving real values(after decimal point also)
    as precision is 10ps (means upto 2 decimal points) that's why after two decimal points it rounded off the value.
    
    
    output-->
    time=0.000ns, clk=0
    time=15.760ns, clk=1
    time=31.520ns, clk=0
    time=47.280ns, clk=1
    time=63.040ns, clk=0

---
+++ time_realtime_precision_concept/_$timeformat_$realtime.v
@@ -0,0 +1,14 @@
+
+`timescale 1ns/10ps
+
+module new ;
+reg clk=0;
+  always #15.757 clk = ~clk;
+  initial
+    $timeformat(-9,3,"ns",1);
+  initial
+    begin
+      $monitor("time=%t, clk=%b",$realtime,clk);
+      #200 $finish;
+    end
+endmodule
